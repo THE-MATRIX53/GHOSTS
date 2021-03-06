26e8fdfe4c2e57e2c6a3c017e047631b81f9dd4c
THE-MATRIX53 <80162898+THE-MATRIX53@users.noreply.github.com>
2021-03-06 16:44:33 +0200

    Add files via upload

---
+++ GHOSTS.py
@@ -0,0 +1,61 @@
+
+import os
+os.system("clear")
+
+
+print(" _____   _   _   _____   _____   _____   _____  ")
+print("/  ___| | | | | /  _  \ /  ___/ |_   _| /  ___/ ")
+print("| |     | |_| | | | | | | |___    | |   | |___  ")
+print("| |  _  |  _  | | | | | \___  \   | |   \___  \ ")
+print("| |_| | | | | | | |_| |  ___| |   | |    ___| | ")
+print("\_____/ |_| |_| \_____/ /_____/   |_|   /_____/ ")
+print("    ")
+print("         ۞ツBY FOUAD AND HUSSEIN FROM 🔥GHOSTS🔥 TEAM   ")
+print("    ")
+gg =input("༼☠༽Type your name↳:  ")
+print(" ")
+print(" ")
+print("꧁☣☠☢꧂WELCOME TO ABU MAHMOUD TOOL: "+  gg)
+print(" ")
+print(" ")
+print("       ⸙✘◕⊱ᴀʙᴜ ᴍᴀʜᴍᴏᴜᴅ🜼 ༼☠༽₂₀₀₄ऌ❥➳͟͞･♡ོ😈")
+
+print("          ")
+print("select (Y) to Downloaded packages or (N) to exit")
+print(" ")
+gg = input("==>: ")
+if gg==("N") :
+    import os
+    os.system("exit")
+    if gg==("n") :
+        import os
+        os.system("exit")
+if gg==("y") :
+    import g
+    g.pk("p")
+if gg==("Y") :
+    import g
+    g.pk("p")
+else:
+    import os
+    os.system("exit")
+import os
+os.system("clear")
+print(" _____   _   _   _____   _____   _____   _____")
+print("/  ___| | | | | /  _  \ /  ___/ |_   _| /  ___/")
+print("| |     | |_| | | | | | | |___    | |   | |___") 
+print("| |_| | | | | | | |_| |  ___| |   | |    ___| |")
+print("\_____/ |_| |_| \_____/ /_____/   |_|   /_____/")
+print(" ")
+print("♧____________________________________________________♧ ")
+print("   OK THE DOWNLOAD IS ♡____D O N E____♡")
+print("   ")
+print("  thank you for use the ◇GHOSTS TEAM◇ tool")
+print("  ")
+
+print("🖤😌By:   ⸙✘◕⊱ᴀʙᴜ ᴍᴀʜᴍᴏᴜᴅ🜼 ༼☠༽₂₀₀₄ऌ❥➳͟͞･♡ོ")
+
+print("the telegram Id: FOADOOO")
+
+import os
+os.system("exit")
---
+++ g.py
@@ -0,0 +1,192 @@
+def pk (p) :
+    import os
+    os.system("pkg update && pkg upgrade")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install git -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install python -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install python2 -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install unzip -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install nano -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install cat -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install curl -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install wget -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install w3m -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install php -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install nodejs -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("apt install golang -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("apt install host -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("apt install havij -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install figlet -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install wget -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install cfengine -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install db -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install tar -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install zip -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install netpb -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install postgresql -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install uftrace -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install python -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install python3 -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install java -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pip install pyfiglet -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install fish -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install ruby -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pip install --upgrade pip")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install perl -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install grep -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install nmap -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install bash -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install clang -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install cowsay -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install tor -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install google -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install sudo -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install wireshark -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install wireshark -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install wgetrc -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install wcalc -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install bmon -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install unrar -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install toilet -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install proot -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install net-tools -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pip install chroot -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install openssl -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install cmatrix -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install openssh")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pip install --upgrade pip")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install macchanger")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pip install colorama")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pip2 install mechanize")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pip2 install requests")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pip install --upgrade pip")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install root-repo -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install unstable-repo -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("pkg install x11-repo -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("apt update && apt upgrade -y")
+    print("♤__________D O N E__________♤")
+    import os
+    os.system("clear")
